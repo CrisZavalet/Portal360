6316aadeae70097d65eb3f5792b8531058806593
Florencia <florenciamacarenasandoval@gmail.com>
2026-02-09 09:50:41 +0100

    add component signup and validations

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,33 +1,48 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators, ReactiveFormsModule,FormBuilder,FormsModule} from '@angular/forms';
 import{ CommonModule } from '@angular/common';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, CommonModule,FormsModule],
+  imports: [ReactiveFormsModule, CommonModule,FormsModule,MatButtonToggleModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+name!: string;
 showPassword = false;
 loginForm!: FormGroup;
 showErrorModal = false;
-showSuccessModal = false;
+showSuccessModal:boolean = false;
 errorMessage = '';
 successMessage = '';
-  isSubmitted = false;
+currentTime: string= '';
+isSubmitted = false;
+active: 'Entrada' | 'Salida' = 'Entrada';
+private clockInterval!: number;
 
 constructor(private fb: FormBuilder) {}
 
 ngOnInit() {
+  const savedMovement = localStorage.getItem('fichaje');
+  this.initClock();
+
   this.loginForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
+    password: new FormControl('', Validators.required),
+    fichaje: new FormControl(savedMovement, Validators.required),
+    dia: new FormControl(''),
+    hora: new FormControl('')
   });
 
 
-  
+
 }
 
+  setActive(value: 'Entrada' | 'Salida') {
+    this.active = value;
+  }
 public get username() {
         return this.loginForm.get('username');
     }
@@ -36,13 +51,21 @@ public get username() {
     }
 
 Login() {
-      this.isSubmitted = true;
+      console.warn(this.loginForm.value);
+
+   const {date, time} = this.getCurrentDateTime();
+  console.log('Date:', date);
+  console.log('Time:', time);
   if (this.loginForm.valid) {
-    const {username, password} = this.loginForm.value;
+    const {username, password,fichaje} = this.loginForm.value;
     console.log('Username:', username);
     console.log('Password:', password);
 
-    this.openModalSuccess('Haz inciado tu jornada exitosamente. \n¡Bienvenido/a ' + this.username + '!');
+    this.loginForm.get('dia')?.setValue(date);
+    this.loginForm.get('hora')?.setValue(time);
+    // console.log('Fichaje:', this.loginForm.get('fichaje')?.value);
+    console.log(this.loginForm.value);
+    this.openModalSuccess('Haz inciado tu jornada exitosamente. \n¡Bienvenido/a ' + this.username?.value + '!');
 
 } else {
     this.loginForm.markAllAsTouched();
@@ -50,17 +73,52 @@ Login() {
       return;
   }
 }
+  getCurrentDateTime(): { date: any; time: any; } {
+     const now = new Date();
+
+  return {
+    date: now.toLocaleDateString('es-ES'),
+    time: now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit' }),
+  };
+  }
+
+initClock() {
+  this.updateClock();
+
+  this.clockInterval = window.setInterval(() => {
+    this.updateClock();
+  }, 1000);
+}
+
+updateClock() {
+  const now = new Date();
+
+  this.currentTime = now.toLocaleTimeString('es-MX', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+
+}
+
+
+onMovementChange(value: 'entrada' | 'salida') {
+  localStorage.setItem('fichaje', value);
+} 
   togglePassword() {
     this.showPassword = !this.showPassword;
   }
 
+
   openModalSuccess(message: string) {
     this.successMessage=message;
     this.showSuccessModal = true;
+
   }
 
   closeModalSuccess() {
     this.showSuccessModal = false;
+    
   }
 
     openModalError(message: string) {
@@ -70,5 +128,6 @@ Login() {
 
   closeModalError() {
     this.showErrorModal = false;
+
   }
 }
