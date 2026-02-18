16fdfbe241c5f584dd3a4589ed06d2d3b3c88861
Florencia <florenciamacarenasandoval@gmail.com>
2026-02-18 13:42:22 +0100

    add cronometro and days

--- src/app/shared/time/time.ts
+++ src/app/shared/time/time.ts
@@ -1,68 +1,27 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-fichaje',
-  imports: [CommonModule],
-  templateUrl: './fichaje.html',
-  styleUrl: './fichaje.css',
+  selector: 'app-time',
+  imports: [],
+  templateUrl: './time.html',
+  styleUrl: './time.css',
 })
-export class Fichaje {
-currentDate: any;
-timer!: any;
-seconds: number = 0; 
-isFichaje: boolean = false;
-tipoFichaje: 'Presencial' | 'Teletrabajo' = 'Presencial';
-intervalId!: any;
+export class Time {
 mediaDiaria = 8;
 horasSemanales = 40
-
+currentDate: any;
 horasRestantes=0;
 private startTime = 0;
 private accumulatedTime = 0;
+seconds: number = 0; 
+
 
 ngOnInit() {
   this.currentDate =new Date().toLocaleDateString('es-ES', {
     weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
 }
-
-// test() {
-//   console.log('CLICK FUNCIONA');
-// }
-
-
-start() {
-  if (this.isFichaje) return;
-  this.isFichaje = true;
-  this.startTime = Date.now()- this.accumulatedTime;
-  this.intervalId = setInterval(() => {
-    this.seconds = Math.floor((Date.now() - this.startTime) / 1000);
-  }, 1000);
-}
-
- pause() {
-     if (!this.isFichaje) return;
-
-  clearInterval(this.intervalId);
-  this.intervalId = null;
-
-  this.accumulatedTime = Date.now() - this.startTime;
-
-  this.isFichaje = false;
-  }
-
-  stop() {
-     clearInterval(this.intervalId);
-  this.intervalId = null;
-
-  this.seconds = 0;
-  this.accumulatedTime = 0;
-  this.startTime = 0;
-  this.isFichaje = false;
-  }
-
- get formattedTime(): string {
+get formattedTime(): string {
     const hrs = Math.floor(this.seconds / 3600);
     const mins = Math.floor((this.seconds % 3600) / 60);
     const secs = this.seconds % 60;
@@ -108,9 +67,5 @@ addOneHour() {
   this.seconds += 3600;
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
-  ngOnDestroy() {
-    clearInterval(this.intervalId);
-  }
 
-  
 }
