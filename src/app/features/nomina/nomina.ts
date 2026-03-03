54da17b3d5f739a9bf893a1d0ccd3ac20d23c63d
Florencia <florenciamacarenasandoval@gmail.com>
2026-03-03 14:04:23 +0100

    add componente nomina

--- src/app/features/nomina/nomina.ts
+++ src/app/features/nomina/nomina.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{ Nominas } from '../../core/interfaces/nomina.interface';
 @Component({
   selector: 'app-nomina',
   imports: [],
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './nomina.css',
 })
 export class Nomina {
+
+exportModalOpen = false;
+nomina: Nominas[] = [
+  { archivo: new Date(2026, 1, 10), periodo: 'Enero 2026', tipo: 'Ordinaria', fechaEmision: new Date(2026, 1, 15), estado: 'Disponible', accion: 'Descargar' },
+  { archivo: new Date(2026, 0, 10), periodo: 'Diciembre 2025', tipo: 'Ordinaria', fechaEmision: new Date(2026, 0, 15), estado: 'Disponible', accion: 'Descargar' },
+  { archivo: new Date(2025, 11, 10), periodo: 'Noviembre 2025', tipo: 'Ordinaria', fechaEmision: new Date(2025, 11, 15), estado: 'Disponible', accion: 'Descargar' }
+];
+
+
+formatearFecha(fecha: Date): string {
+  return new Date(fecha).toLocaleDateString('es-ES');
+}
+
+
+openExportModal() {
+  this.exportModalOpen = true;  
+}
+closeExportModal() {
+  this.exportModalOpen = false;  
+}
 
 }
