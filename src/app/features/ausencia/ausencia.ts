290d2733a1e6981f06487aea8dbe516f95de0e22
Florencia <florenciamacarenasandoval@gmail.com>
2026-03-13 10:08:39 +0100

    add form solicitar ausencias

--- src/app/features/ausencia/ausencia.ts
+++ src/app/features/ausencia/ausencia.ts
@@ -5,18 +5,32 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import { Holiday } from '../../core/services/holiday';
+import { CalendarEvent } from '../../core/interfaces/calendario.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ausencia',
-  imports: [FullCalendarModule],
+  imports: [FullCalendarModule, CommonModule],
   templateUrl: './ausencia.html',
   styleUrl: './ausencia.css',
 })
 export class Ausencia {
 
+  constructor(private holidayService: Holiday) {
+  }
+exportModalOpen = false;
+modalOpen = false;
+selectAbsenceType=[ 'Baja Boda', 'Baja Larga', 'Baja Paternidad','Incapacidad Temporal','Permiso de operacion','Vacaciones' ];
+durationType: 'hours' | 'day' | 'days' | null = null;
+
+
+holidays:any[] = [];
   year = new Date().getFullYear();
 
   months: (number | null)[][] = [];
+events: CalendarEvent[] = [];
+  selectedHoliday: {date:string,name:string} | null = null;
 
   monthNames = [
     'Enero','Febrero','Marzo','Abril','Mayo','Junio',
@@ -25,15 +39,32 @@ export class Ausencia {
 
   weekDays = ['L','M','X','J','V','S','D'];
 
-  events = [
-    '2026-01-01',
-    '2026-01-06',
-    '2026-04-03',
-    '2026-05-01'
-  ];
  ngOnInit() {
     this.generateCalendar();
+    this.loadHolidays();
+
+    this.events.push(
+  {
+    date: `${this.year}-07-10`,
+    name: 'Vacaciones',
+    type: 'vacation'
+  },
+  {
+    date: `${this.year}-03-18`,
+    name: 'Baja médica',
+    type: 'sick'
+  },
+  {
+    date: `${this.year}-04-22`,
+    name: 'Ausencia',
+    type: 'absence'
   }
+);
+  }
+
+setDuration(type: 'hours' | 'day' | 'days') {
+  this.durationType = type;
+}
 
   generateCalendar() {
 
@@ -59,25 +90,141 @@ export class Ausencia {
 
   }
 
-  isEvent(day: number | null, month:number){
-
-    if(!day) return false;
-
-    const date =
-      `${this.year}-${String(month+1).padStart(2,'0')}-${String(day).padStart(2,'0')}`;
-
-    return this.events.includes(date);
-
-  }
-
- changeYear(step:number){
+changeYear(step:number){
 
   this.year += step;
 
   this.months = [];
+  this.events = [];
 
   this.generateCalendar();
+  this.loadHolidays();
 
+}
+
+
+  loadHolidays(){
+
+    this.holidayService.getHolidays(this.year).subscribe((data:any)=>{
+
+    this.holidays = data.map((h:any)=>({
+      date: h.startDate,
+      name: h.name?.[0]?.text || 'Festivo',
+            type: 'holiday'
+
+    }));
+
+    this.holidays.push({
+      date: `${this.year}-05-15`,
+      name: 'San Isidro',
+            type: 'holiday'
+
+    });
+
+     this.events = [...this.events, ...this.holidays];
+
+  });
+    
+
+  }
+
+isHoliday(day:number | null, month:number){
+
+   if(!day) return false;
+
+  const date =
+  `${this.year}-${String(month+1).padStart(2,'0')}-${String(day).padStart(2,'0')}`;
+
+  return this.holidays.some(h => h.date === date);  
+
+  }
+
+
+  getHolidayName(day:number | null, month:number){
+
+  if(!day) return '';
+
+  const date =
+  `${this.year}-${String(month+1).padStart(2,'0')}-${String(day).padStart(2,'0')}`;
+
+  const holiday = this.holidays.find(h => h.date === date);
+
+  return holiday ? holiday.name : '';
+
+}
+
+openHoliday(day:number | null, month:number){
+
+  if(!day) return;
+
+  const date =
+  `${this.year}-${String(month+1).padStart(2,'0')}-${String(day).padStart(2,'0')}`;
+
+  const holiday = this.holidays.find(h => h.date === date);
+
+  if(holiday){
+    this.selectedHoliday = holiday;
+
+    const modal = document.getElementById('holiday_modal') as HTMLDialogElement;
+    modal.showModal();
+  }
+
+}
+
+getEvent(day:number | null, month:number){
+
+  if(!day) return null;
+
+  const date =
+  `${this.year}-${String(month+1).padStart(2,'0')}-${String(day).padStart(2,'0')}`;
+
+  return this.events.find(e => e.date === date);
+
+}
+
+selectedEvent: CalendarEvent | null = null;
+
+openEvent(day:number | null, month:number){
+
+  const event = this.getEvent(day,month);
+
+  if(!event) return;
+
+  this.selectedEvent = event;
+
+  const modal = document.getElementById('event_modal') as HTMLDialogElement;
+  modal.showModal();
+
+}
+
+openExportModal() {
+  this.exportModalOpen = true;  
+}
+
+closeExportModal() {
+  this.exportModalOpen = false;  }
+
+// hoy
+isToday(day:number | null, month:number){
+
+  if(!day) return false;
+
+  const today = new Date();
+
+  return (
+    today.getFullYear() === this.year &&
+    today.getMonth() === month &&
+    today.getDate() === day
+  );
+
+}
+
+openModal() {
+  this.modalOpen = true;
+}
+
+closeModal() {
+  this.modalOpen = false;
 }
 
 }
