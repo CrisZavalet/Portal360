50ec4c75ba4329787402b5ec83aea3858fced8a6
Florencia <florenciamacarenasandoval@gmail.com>
2026-07-06 13:43:54 +0200

    antes de un pull

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -44,16 +44,10 @@ export const routes: Routes = [
       { path: 'profile', component: Perfil, canActivate: [RoleGuard],data: { roles: [Role.EMPLEADO, Role.ADMIN, Role.RRHH] } },
       { path: 'config', component: Configurations, canActivate: [RoleGuard],data: { roles: [Role.EMPLEADO, Role.ADMIN, Role.RRHH] } },
       {path:'inbox', component:BandejaEntrada, canActivate: [RoleGuard],data: { roles: [Role.EMPLEADO, Role.ADMIN, Role.RRHH] } },
-<<<<<<< HEAD
-      {path:'clock-in/:id', component:FichajeEmpleado, canActivate: [RoleGuard],data: { roles: [ Role.ADMIN, Role.RRHH] } },
-      {path:'paysheet/:id', component:NominaEmpleado, canActivate: [RoleGuard],data: { roles: [Role.ADMIN, Role.RRHH] } },
-      {path:'documents/:id', component:DocumentosEmpleado, canActivate: [RoleGuard],data: { roles: [Role.ADMIN, Role.RRHH] } },
-=======
       {path:'management-clock-in/:id', component:FichajeEmpleado, canActivate: [RoleGuard],data: { roles: [ Role.ADMIN, Role.RRHH] } },
       {path:'management-paysheet/:id', component:NominaEmpleado, canActivate: [RoleGuard],data: { roles: [Role.ADMIN, Role.RRHH] } },
       {path:'management-documents/:id', component:DocumentosEmpleado, canActivate: [RoleGuard],data: { roles: [Role.ADMIN, Role.RRHH] } },
       {path:'view-time', component:ViewAdmin },
->>>>>>> 03aa3be (antes de un pull)
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
     ]
   },
