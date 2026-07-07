7eeae44f1b4d1688533c16e159f21c21b930a38d
Cristhian Trinidad <cristhia.trinidad.it@moqapi.com>
2026-07-07 12:43:12 +0200

    Añade endpoints y DTOs para gestión de fichajes

--- Backend/portal360/src/main/java/com/portal/portal360/repository/FichajeRepository.java
+++ Backend/portal360/src/main/java/com/portal/portal360/repository/FichajeRepository.java
@@ -4,6 +4,10 @@ import com.portal.portal360.model.Fichaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 public interface FichajeRepository extends JpaRepository<Fichaje, Integer> {
     
@@ -13,7 +17,18 @@ public interface FichajeRepository extends JpaRepository<Fichaje, Integer> {
     // Y aquí usamos 'Date' porque así se llama tu variable en Fichaje.java
     List<Fichaje> findByIdEmployeeAndDateBetween(Integer idEmployee, LocalDate startDate, LocalDate endDate);
 
+    List<Fichaje> findByIdEmployeeOrderByDateDescStartHourDesc(Integer idEmployee);
+
+    Optional<Fichaje> findFirstByIdEmployeeAndEndHourIsNullOrderByDateDescStartHourDesc(Integer idEmployee);
+
+    
+    @Query("""
+SELECT f
+FROM Fichaje f
+JOIN FETCH f.employee
+WHERE f.date = :date
+""")
+List<Fichaje> findTodayClockings(LocalDate date);
 
 
-Optional<Fichaje> findFirstByIdEmployeeAndEndHourIsNullOrderByDateDescStartHourDesc(Integer idEmployee);
 }
