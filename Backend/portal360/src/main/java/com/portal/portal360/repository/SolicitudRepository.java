cd757548e5cf00b525f026bbd95e00257080bfd0
Cristhian Trinidad <cristhia.trinidad.it@moqapi.com>
2026-08-19 12:27:17 +0200

    solicitud id empleado solo

--- Backend/portal360/src/main/java/com/portal/portal360/repository/SolicitudRepository.java
+++ Backend/portal360/src/main/java/com/portal/portal360/repository/SolicitudRepository.java
@@ -1,9 +1,13 @@
 package com.portal.portal360.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.portal.portal360.model.Solicitud;
 
 public interface SolicitudRepository extends JpaRepository<Solicitud, Integer> {
+
+    List<Solicitud> findByIdEmployee(Integer idEmployee);
 
 }
